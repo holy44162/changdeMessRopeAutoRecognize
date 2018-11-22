f2726008f30867f9ec638159cb29f1477968334d
James <James@DESKTOP-1A4AIF4>
2018-11-22 15:39:08 +0800

    add code files.

---
+++ fun_trainMLParasByGMM.m
@@ -0,0 +1,117 @@
+function [bestPara,paraLog,dataMLOutput,gaussianParaOutput] = fun_trainMLParasByGMM(maxHogSize,maxImgEdge,heightBias,widthBias,numImgEdgeStep,numHogSizeStep,trainFolderName,CVFolderName,testFolderName,featureType)
+
+minImgEdge = 0;
+
+
+if numImgEdgeStep == 0
+    imgEdgeSteps = 0;
+else
+    stepSizeImgEdge = (maxImgEdge - minImgEdge) / numImgEdgeStep;
+    imgEdgeSteps = maxImgEdge:-stepSizeImgEdge:minImgEdge;
+end
+
+minHogSize = 8;
+
+
+if numHogSizeStep == 0
+    hogSizeSteps = maxHogSize;
+else
+    % hided by Holy 1811221450
+%     stepSizeHogSize = (maxHogSize - minHogSize) / numHogSizeStep;
+%     hogSizeSteps = maxHogSize:-stepSizeHogSize:minHogSize;
+    % end of hide 1811221450
+    
+    % added by Holy 1811221452
+    maxNumHogSize = log2(maxHogSize)-log2(minHogSize)+1;
+    if numHogSizeStep > maxNumHogSize
+        numHogSizeStep = maxNumHogSize;
+    end
+    hogSizeSteps = 2.^(log2(minHogSize):(log2(minHogSize)+numHogSizeStep-1));
+    % end of addition 1811221452
+end
+
+gaborsBinHogFeatureType = 'gaborsBinHog';
+
+bestPara = num2cell(zeros(1,6));
+paraLog = [];
+iProgress = 1;
+
+for imgEdge = imgEdgeSteps
+    progressbar(iProgress,numImgEdgeStep+1);
+    heightImgEdge = round(heightBias + imgEdge);
+    widthImgEdge = round(widthBias + imgEdge);
+    
+    if contains(featureType, gaborsBinHogFeatureType,'IgnoreCase',true)
+        for hogSize = hogSizeSteps
+            hogSize1 = round(hogSize);
+            % train
+            dataML = realWindingFeatureDataGen(trainFolderName,hogSize1,heightImgEdge,widthImgEdge,featureType);
+            
+            % CV
+            dataML = realWindingFeatureDataGen(CVFolderName,hogSize1,heightImgEdge,widthImgEdge,featureType,dataML);
+            
+            % test
+            dataML = realWindingFeatureDataGen(testFolderName,hogSize1,heightImgEdge,widthImgEdge,featureType,dataML);
+            
+            % get best parameters
+            numDim = size(dataML.X,2);
+            resultMatrix = zeros(numDim,4);
+            for i = 1:numDim
+                gaussianPara = fun_trainMultiplyGaussian(dataML,i);
+                
+                [resultMatrix(i,1),resultMatrix(i,2),resultMatrix(i,3)] = fun_testMultiplyGaussian(dataML,i,gaussianPara);
+                resultMatrix(i,4) = i;
+            end
+            sortedResult = sortrows(resultMatrix,'descend');
+            
+            % remove lines with nan elements
+            nanTag = isnan(sortedResult);
+            nanInd = any(nanTag,2);
+            sortedResult(nanInd,:) = [];
+            
+            % perform feature selection
+            maxF1Row = num2cell(sortedResult(1,:));
+            maxF1Row = [maxF1Row {hogSize1} {imgEdge}];
+            featureIDs = maxF1Row(4);
+            resultCell = cell(1,4);
+            selectedIDs = featureIDs;
+            for i = 1:size(sortedResult,1)
+                if i > 1
+                    diff = length(featureIDs{1}) - length(selectedIDs{1});
+                    if diff == 0
+                        break;
+                    else
+                        selectedIDs = featureIDs;
+                    end
+                end
+                for j = 1:size(sortedResult,1)
+                    if sum(selectedIDs{1}==sortedResult(j,4)) == 1
+                        continue;
+                    end
+                    dimIDs = [selectedIDs{1} sortedResult(j,4)];
+                    gaussianPara = fun_trainMultiplyGaussian(dataML,dimIDs);
+                    [resultCell{1,1},resultCell{1,2},resultCell{1,3}] = fun_testMultiplyGaussian(dataML,dimIDs,gaussianPara);
+                    resultCell{1,4} = dimIDs;
+                    
+                    if resultCell{1,1} == 1
+                        bestPara = [bestPara;[resultCell {hogSize1} {imgEdge}]];
+                    end
+                                        
+                    if resultCell{1,1} > maxF1Row{1}
+                        maxF1Row(1:4) = resultCell;
+                        featureIDs = maxF1Row(4);
+                        
+                        if maxF1Row{1} > bestPara{1,1}
+                            bestPara = maxF1Row;
+                            dataMLOutput = dataML;
+                            gaussianParaOutput = gaussianPara;
+                        end                        
+                    end
+                end
+            end
+            paraLog = [paraLog;maxF1Row];
+        end
+    end
+    iProgress = iProgress + 1;
+end
+end
---
+++ getGMMBestPara.m
@@ -0,0 +1,32 @@
+clear;
+tStart = tic;
+functionPath = 'm:\files\files\phd\functions\';
+addpath(functionPath);
+
+addpath([functionPath 'toolbox_general']);
+addpath([functionPath 'SoundZone_Tools-master']);
+addpath([functionPath 'parfor_progress']);
+addpath([functionPath 'Texture-Segmentation-using-Gabor-Filters']);
+
+maxHogSize = 64;
+
+maxImgEdge = 30;
+
+heightBias = 0;
+widthBias = 0;
+
+numImgEdgeStep = 0;
+numHogSizeStep = 2;
+
+trainFolderName = 'd:\data_seq\smallWinding1\train\imgs\';
+CVFolderName = 'd:\data_seq\smallWinding1\CV1\imgs\';
+testFolderName = 'd:\data_seq\smallWinding1\test\imgs\';
+
+featureType = 'gaborsBinHog';
+
+[bestPara,paraLog,dataMLOutput,gaussianParaOutput] = fun_trainMLParasByGMM(maxHogSize,maxImgEdge,heightBias,widthBias,numImgEdgeStep,numHogSizeStep,trainFolderName,CVFolderName,testFolderName,featureType);
+save('bestPara.mat','bestPara','paraLog','dataMLOutput','gaussianParaOutput','-v7.3');
+
+totalElapsedTime = toc(tStart);
+disp(['total time: ' num2str(totalElapsedTime) ' sec']);
+disp(['total time: ' num2str(totalElapsedTime/60) ' min']);
