a65341859e673d987e6829730b05dce46fbca1a4
holy44162 <holy44162@gmail.com>
2019-01-08 09:25:58 +0800

    add build test video code.

---
+++ buildTestVideo.m
@@ -0,0 +1,16 @@
+clear;
+tStart = tic;
+functionPath = 'm:\files\files\phd\functions\';
+% functionPath = 'd:\baiduSyn\files\phd\functions\';
+addpath(functionPath);
+addpath([functionPath 'toolbox_general']);
+
+builtVideoPathName = 'd:\data_seq\changdeWinding\winding2\test.mp4';
+normalFramesPath = 'd:\data_seq\changdeWinding\winding2\test\imgs\';
+messFramesPath = 'd:\data_seq\changdeWinding\winding2\mess\test\';
+
+fun_buildVideo(builtVideoPathName, normalFramesPath, messFramesPath);
+
+totalElapsedTime = toc(tStart);
+disp(['total time: ' num2str(totalElapsedTime) ' sec']);
+disp(['total time: ' num2str(totalElapsedTime/60) ' min']);
