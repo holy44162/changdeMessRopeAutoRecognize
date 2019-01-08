32a1bbf0cec27f7efaeb78dc300694f863150c27
holy44162 <holy44162@gmail.com>
2019-01-08 11:06:08 +0800

    update.

--- buildTestVideo.m
+++ buildTestVideo.m
@@ -6,10 +6,13 @@ addpath(functionPath);
 addpath([functionPath 'toolbox_general']);
 
 builtVideoPathName = 'd:\data_seq\changdeWinding\winding2\test.mp4';
-normalFramesPath = 'd:\data_seq\changdeWinding\winding2\test\imgs\';
+normalFramesPath = 'd:\data_seq\changdeWinding\winding2\video\test\imgs\';
 messFramesPath = 'd:\data_seq\changdeWinding\winding2\mess\test\';
 
-fun_buildVideo(builtVideoPathName, normalFramesPath, messFramesPath);
+fps = 25;
+portionNum = 7;
+
+fun_buildVideo(builtVideoPathName, normalFramesPath, messFramesPath, fps, portionNum);
 
 totalElapsedTime = toc(tStart);
 disp(['total time: ' num2str(totalElapsedTime) ' sec']);
