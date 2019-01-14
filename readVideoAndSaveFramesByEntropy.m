0e68690ba0df12f3380eec5b517c33aea511c48b
holy44162 <holy44162@gmail.com>
2019-01-14 12:03:15 +0800

    update.

--- readVideoAndSaveFramesByEntropy.m
+++ readVideoAndSaveFramesByEntropy.m
@@ -9,7 +9,12 @@ addpath([functionPath 'messRopeFunctions']);
 
 % videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\data_mess_all_2019_01_07_17_27_22(1).mp4';
 % videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000022000000(1).mp4';
-videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000007000000(1).mp4';
+% videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000007000000(1).mp4';
+% videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000028000200(1)_190107.mp4';
+% videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000028000000(1)_190107.mp4';
+% videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000025000000(1)_190107.mp4';
+% videoPathName = 'd:\data\建起常德新工业园Winding_data\190109\winding2\00000000081000100(1)_190109.mp4';
+videoPathName = 'd:\data\建起常德新工业园Winding_data\190109\winding2\00000000081000000(1)_190109.mp4';
 
 [pathName,fileName,fileExt] = fileparts(videoPathName);
 
@@ -20,7 +25,7 @@ upDirName = getUpLevelPath(imgsPath, 1);
 rectFilePathName = fullfile(upDirName, 'rect_anno.txt');
 rotateFilePathName = fullfile(upDirName, 'angle_rotate.txt');
 
-entropyThreshold = 1e-3;
+entropyThreshold = 1e-4;
 
 fun_readVideoAndSaveFramesByEntropy(videoPathName, imgsPath, rectFilePathName, rotateFilePathName, entropyThreshold);
 toc
--- testEntropyThreshold.m
+++ testEntropyThreshold.m
@@ -7,7 +7,9 @@ addpath([functionPath 'toolbox_general']);
 addpath([functionPath 'messRopeFunctions']);
 
 
-videoPathName = 'd:\data\190104建起常德新工业园Winding_data\2019-01-04\00000000046000100_20190107120516.mp4';
+% videoPathName = 'd:\data\190104建起常德新工业园Winding_data\2019-01-04\00000000046000100_20190107120516.mp4';
+% videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000007000000(1).mp4';
+videoPathName = 'd:\data\建起常德新工业园Winding_data\190107\00000000028000000(1).mp4';
 maxFrameNum = 1000;
 
 [pathName,fileName,fileExt] = fileparts(videoPathName);
