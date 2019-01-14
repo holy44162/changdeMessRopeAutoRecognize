772666dda582a845b5b203b5e578618774b41d6a
holy44162 <holy44162@gmail.com>
2019-01-14 17:57:14 +0800

    update.

--- testGaborFeatures.m
+++ testGaborFeatures.m
@@ -5,18 +5,44 @@ addpath(functionPath);
 addpath([functionPath 'toolbox_general']);
 addpath([functionPath 'Texture-Segmentation-using-Gabor-Filters']);
 
-imgPathName = 'd:\data_seq\changdeWinding\winding2\mess\img00001.jpg'; % messs
+% imgPathName = 'd:\data_seq\changdeWinding\winding2\mess\img00001.jpg'; % mess
+% imgPathName = 'd:\data_seq\changdeWinding\winding2\mess\img05916.jpg'; % mess
+% imgPathName = 'd:\data_seq\changdeWinding\winding2\test\imgs\img00001.jpg'; % normal
+imgPathName = 'd:\data_seq\changdeWinding\winding2\train\imgs\img15830.jpg'; % normal
+% imgPathName = 'd:\data_seq\smallWinding1\positiveSamples\test\06-Nov-2018-11-28-50\img00030.jpg'; % mess
 
-rectFilePathName = 'rect_anno.txt';
-rotateFilePathName = 'angle_rotate.txt';
+rectFilePathName = 'rect_anno_changde2.txt';
+rotateFilePathName = 'angle_rotate_changde2.txt';
+% rectFilePathName = 'rect_anno_smallWinding.txt';
+% rotateFilePathName = 'angle_rotate_smallWinding.txt';
 
-inputImg = adapthisteq(inputImg,'NumTiles',[16,16]);
-inputImg = adapthisteq(inputImg,'NumTiles',[8,8]);
-inputImg = adapthisteq(inputImg,'NumTiles',[4,4]);
+if exist(rectFilePathName, 'file')
+    rectWinding = dlmread(rectFilePathName);
+    rectWinding = round(rectWinding);
+else
+    error(['couldn''t find ' rectFilePathName]);
+end
 
-inputImg = medfilt2(inputImg);
+if exist(rotateFilePathName, 'file')
+    theta = dlmread(rotateFilePathName);
+else
+    error(['couldn''t find ' rotateFilePathName]);
+end
 
-[~, Nc, ~] = size(inputImg);
+inputImg = imread(imgPathName);
+if (size(inputImg,3) ~= 1)
+  inputImg = rgb2gray(inputImg);
+end
+
+imgRected = fun_rotateRect(inputImg, theta, rectWinding);
+
+imgRected = adapthisteq(imgRected,'NumTiles',[16,16]);
+imgRected = adapthisteq(imgRected,'NumTiles',[8,8]);
+imgRected = adapthisteq(imgRected,'NumTiles',[4,4]);
+
+imgRected = medfilt2(imgRected);
+
+[~, Nc, ~] = size(imgRected);
 gamma = 1;
 b = 1;
 %     Theta = 0:pi/6:pi-pi/6;
@@ -38,4 +64,15 @@ J = (2.^(0:log2(Nc/8)) - .5) ./ Nc;
 F = [ (.25 - J) (.25 + J) ]; F = sort(F); Lambda = 1 ./ F;
 Lambda = Lambda(end-1:end);
 % ----------------------------
-[Xtest,bwImg] = GaborTextureSegment(inputImg, gamma, Lambda, b, Theta, phi, shape, hogSize);
+debugTag = true;
+Sensitivity = 0.88;
+[Xtest,bwImg,smoothedGaborImgs] = fun_GaborTextureSegment(imgRected, gamma, Lambda, b, Theta, phi, shape, Sensitivity, debugTag);
+
+figure('NumberTitle','Off','Name','Gabor feature imgs');
+for i = 1:length(Lambda)
+    for j = 1:length(Theta)      
+        subplot(length(Lambda),length(Theta),(i-1)*length(Theta)+j);        
+        imshow(smoothedGaborImgs(:,:,(i-1)*length(Theta)+j),[]);
+    end
+end
+figure, imshow(bwImg);
